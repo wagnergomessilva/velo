8a6920da3decb28a27c477e62072d8c6e8ad01ad
Wagner <wag.gomes2015@gmail.com>
2026-01-22 09:45:02 -0300

    Mudando o título do cenário

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('webapp deve estar online', async ({ page }) => {
+test('A webapp deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173')
 
   // Expect a title "to contain" a substring.
