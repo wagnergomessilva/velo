e8ece5f4881ffb6686aa9e6ee52834cf56b74787
Wagner <wag.gomes2015@gmail.com>
2026-01-26 09:51:08 -0300

    Atualize a configuração do Playwright com um tempo limite de 10 segundos e modifique o teste de pesquisa de ordem para usar um seletor mais específico.

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -10,11 +10,13 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
     // Act
-    await page.getByTestId('search-order-id').fill('VLO-4PXWIM')
+    //await page.getByTestId('search-order-id').fill('VLO-4PXWIM')
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-4PXWIM')
     await page.getByTestId('search-order-button').click()
 
     // Assert
-    await expect(page.getByTestId('order-result-id')).toBeVisible()
+    
+    await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10_000 })
     await expect(page.getByTestId('order-result-id')).toContainText('VLO-4PXWIM')
     await expect(page.getByTestId('order-result-status')).toBeVisible()
     await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
