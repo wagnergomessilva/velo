23c1459439244e71613b9592a65e52c344484db2
Wagner <wag.gomes2015@gmail.com>
2026-02-06 11:04:55 -0300

    Aula: IA no processo de testes

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -142,6 +142,55 @@ test.describe('Consulta de pedido', () => {
             `);
     })
 
+    test('deve consultar um pedido em análise', async ({ page }) => {
+
+        //Massa de teste
+        //const order = 'VLO-H18ESA'
+        const order = {
+            number: 'VLO-6SDLHZ',
+            status: 'EM_ANALISE',
+            color: 'Lunar White',
+            wheels: 'aero Wheels',
+            customer: {
+                name: 'Joao Silva',
+                email: 'joao@velo.dev',
+            },
+            payment: 'À Vista',
+        }
+
+        await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+        await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${order.number}
+            - img
+            - text: ${order.status}
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: ${order.color}
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: ${order.wheels}
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: ${order.customer.name}
+            - paragraph: Email
+            - paragraph: ${order.customer.email}
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: ${order.payment}
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
+    })
+
     test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
 
         //Massa de teste
